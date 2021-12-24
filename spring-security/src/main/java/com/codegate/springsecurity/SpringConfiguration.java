756454a3473eda47f384559ee8533893c96ec4c6
Otávio Silva <otaviosilva2632@gmail.com>
2021-12-23 22:15:11 -0300

    feat: add roles and permissions

--- spring-security/src/main/java/com/codegate/springsecurity/SpringConfiguration.java
+++ spring-security/src/main/java/com/codegate/springsecurity/SpringConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -19,13 +20,21 @@ public class SpringConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().authenticated().and()
-        .formLogin().loginPage("/login").permitAll();
+    http.authorizeRequests()
+        .antMatchers("/").access("hasAnyAuthority('USERS', 'ADMIN')")
+        .antMatchers("/admin").access("hasAuthority('ADMIN')")
+        .anyRequest().authenticated().and()
+        .formLogin().loginPage("/login").permitAll()
+        .and()
+        .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+        .logoutSuccessUrl("/login").permitAll();
   }
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication().withUser("user")
-        .password(passwordEncoder().encode("password")).authorities("USER");
+    auth.inMemoryAuthentication()
+        .withUser("otavio").password(passwordEncoder().encode("otavio")).authorities("ADMIN")
+        .and()
+        .withUser("user").password(passwordEncoder().encode("user")).authorities("USER");
   }
 }
