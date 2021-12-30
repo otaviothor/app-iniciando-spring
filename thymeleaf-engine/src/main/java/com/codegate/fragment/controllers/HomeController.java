d1a0a023e48b49b76c2b59b93bdf6228c0b68cff
Otávio Silva <otaviosilva2632@gmail.com>
2021-12-30 12:05:43 -0300

    feat: create home controller

---
+++ thymeleaf-engine/src/main/java/com/codegate/fragment/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package com.codegate.fragment.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "index";
+  }
+
+  @RequestMapping("/blog")
+  public String blog() {
+    return "blog";
+  }
+
+  @RequestMapping("/contato")
+  public String contact() {
+    return "contact";
+  }
+}
