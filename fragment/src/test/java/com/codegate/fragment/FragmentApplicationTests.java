5a3ea8b435abe7c270e1c032594e92ea0c2a6c05
Otávio Silva <otaviosilva2632@gmail.com>
2021-12-30 12:01:03 -0300

    feat: create project

---
+++ fragment/src/test/java/com/codegate/fragment/FragmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegate.fragment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
